a084a0b60d2fe5e4c6b3fbb1a08e515428fc9f0f
Ayta Arylova <ayta.arylova@gmail.com>
2021-04-18 23:03:46 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import './js/fetchCountries';
+import './js/fetchAPI';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
---
+++ src/js/fetchAPI.js
@@ -0,0 +1,67 @@
+import countryCardTmpl from '../template/countries.hbs';
+import API from './fetchCountries';
+import countryListTmpl from '../template/list.hbs';
+import debounce from 'lodash.debounce';
+import { error } from '@pnotify/core';
+
+const inputField = document.querySelector('.field');
+const cardContainer = document.querySelector('.js-card-container');
+const listCountry = document.querySelector('.list-country');
+
+let form = '';
+inputField.addEventListener('input', debounce(onSearchCountry, 500));
+function onSearchCountry(e) {
+  resetContent();
+  form = e.target.value;
+  renderMarkUp(form);
+}
+function renderMarkUp(data) {
+  if (data !== '') {
+    API.fetchCountries(data).then(onInputChange).catch(onFetchError);
+  }
+}
+
+function onInputChange(data) {
+  if (data.length === 1) {
+    renderCountryMarkUp(data);
+    deleteError();
+  }
+  if (data.length > 1 && data.length < 11) {
+    renderCountriesList(data);
+    deleteError();
+  }
+  if (data.length > 10) {
+    error({
+      text: 'Too many matches found! Please enter a more specific query!',
+      // delay: 5000,
+    });
+  }
+}
+function onFetchError() {
+  error({
+    title: 'Oh No!',
+    text: 'Error!',
+    // delay: 5000,
+  });
+}
+
+function deleteError() {
+  const errorMessage = document.querySelector('.pnotify');
+  if (document.body.contains(errorMessage)) {
+    errorMessage.style.display = 'none';
+  }
+}
+
+function renderCountriesList(countries) {
+  listCountry.innerHTML = countryListTmpl(countries);
+}
+
+function renderCountryMarkUp(countries) {
+  cardContainer.innerHTML = countryCardTmpl(countries[0]);
+}
+
+function resetContent() {
+  listCountry.innerHTML = '';
+  cardContainer.innerHTML = '';
+  deleteError();
+}
